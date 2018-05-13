e5c4cd6d4d15dac7ce7e7beec2d5ae7739c60561
Mario Iliasi <marioiliasi96@gmail.com>
2018-05-13 21:49:24 +0300

    whatever

--- src/main/java/com/pad/bgvi/server/ServletLifecycleListener.java
+++ src/main/java/com/pad/bgvi/server/ServletLifecycleListener.java
@@ -1,8 +1,5 @@
 package com.pad.bgvi.server;
 
-import java.net.MalformedURLException;
-import java.rmi.AlreadyBoundException;
-import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -13,8 +10,11 @@ import javax.servlet.ServletContextListener;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import com.pad.bgvi.common.RMIServerUtil;
+import com.pad.bgvi.server.model.Product;
 import com.pad.bgvi.server.rmi.ShopImpl;
 import com.pad.bgvi.server.utils.HibernateUtil;
 
@@ -38,14 +38,61 @@ public class ServletLifecycleListener implements ServletContextListener{
 		//creating transaction object
 		Transaction t=session.beginTransaction();
 		Product p = new Product();
-		p.setName("whatever");
-		p.setDescription("fuck off :*");
+		p.setName("Pantaloni dans damă");
+		p.setDescription("Pantalon cu talie joasă și croială reglabilă. Modern și confortabil.");
 		p.setCategoryType(1);
 		p.setImage("assets/dans_dama.jpg");
-		p.setPrice(1000000);
-		p.setQuantity(234);
+		p.setPrice(27);
+		p.setQuantity(0);
 		session.persist(p);
+		
+		Product p2 = new Product();
+		p2.setName("Tricou drumeție damă");
+		p2.setDescription("Tricoul evacuează eficient transpirația și evită mirosurile neplăcute. Ideal pentru primele drumeții la munte.");
+		p2.setCategoryType(1);
+		p2.setImage("assets/drumetie.jpg");
+		p2.setPrice(25);
+		p2.setQuantity(2);
+		session.persist(p2);
+		
+		Product p3 = new Product();
+		p3.setName("Încălțăminte drumeție bărbați");
+		p3.setDescription("Talpă din cauciuc ce asigură rezistenţă şi aderenţă optimă. Faţa mid oferă susţinere, pentru o drumeţie sigură la munte.");
+		p3.setCategoryType(2);
+		p3.setImage("assets/incaltaminte_drumetie_barbati.jpg");
+		p3.setPrice(80);
+		p3.setQuantity(2);
+		session.persist(p3);
+		
+		Product p4 = new Product();
+		p4.setName("Poante de piele");
+		p4.setDescription("Material din piele pentru mișcare mai bună a boltei plantare. Bandă din nailon ce asigură flexibilitate și eleganță.");
+		p4.setCategoryType(2);
+		p4.setImage("assets/poante_piele.jpg");
+		p4.setPrice(45);
+		p4.setQuantity(3);
+		session.persist(p4);
+		
+		Product p5 = new Product();
+		p5.setName("Racheta tenis BABOLAT");
+		p5.setDescription("Noua rachetă BABOLAT Pure Drive oferă un echilibru perfect între lovituri puternice și control.");
+		p5.setCategoryType(3);
+		p5.setImage("assets/babolat.jpg");
+		p5.setPrice(110);
+		p5.setQuantity(7);
+		session.persist(p5);
+		
+		Product p6 = new Product();
+		p6.setName("Bicicleta dama");
+		p6.setDescription("Bicicleta de oras pentru dame.");
+		p6.setCategoryType(3);
+		p6.setImage("assets/bicicleta.jpg");
+		p6.setPrice(250);
+		p6.setQuantity(2);
+		session.persist(p6);
+		
 		t.commit();
+		
 		session.close();*/
 	}
 	private void initRMIServer() {
@@ -55,7 +102,7 @@ public class ServletLifecycleListener implements ServletContextListener{
 			// System.out.println("Server is ready");
 			// reg.rebind(RMIServerUtil.URI, server);
 			//Remote r = new ShopImpl();
-			reg.rebind(RMIServerUtil.URI, server);
+			reg.rebind("//192.168.43.94/server-management", server);
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
--- src/main/java/com/pad/bgvi/server/rmi/ShopImpl.java
+++ src/main/java/com/pad/bgvi/server/rmi/ShopImpl.java
@@ -9,8 +9,13 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 import com.pad.bgvi.common.model.Article;
 import com.pad.bgvi.common.rmi.Shop;
+import com.pad.bgvi.server.model.Product;
+import com.pad.bgvi.server.utils.HibernateUtil;
 
 public class ShopImpl extends UnicastRemoteObject implements Shop{
 
@@ -30,12 +35,12 @@ public class ShopImpl extends UnicastRemoteObject implements Shop{
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
                 Article a = new Article();
-                a.setName(rs.getString(1));
-                a.setType(rs.getInt(2));
-                a.setDescription(rs.getString(3));
-                a.setImage(rs.getString(4));
-                a.setPrice(rs.getDouble(5));
-                a.setQuantity(rs.getInt(6));
+                a.setName(rs.getString(9));
+                a.setType(rs.getInt(6));
+                a.setDescription(rs.getString(7));
+                a.setImage(rs.getString(8));
+                a.setPrice(rs.getDouble(10));
+                a.setQuantity(rs.getInt(11));
                 list.add(a);
             }
             connection.close();
@@ -44,4 +49,18 @@ public class ShopImpl extends UnicastRemoteObject implements Shop{
         }
         return list;
     }
+
+	public void buyArticles(List<Article> articles) throws RemoteException {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		for(Article art : articles) {
+			Product p = (Product) session.get(Product.class, art.getId());
+			int diff = p.getQuantity() - art.getQuantity();
+			p.setQuantity(p.getQuantity() - diff);
+			session.persist(p);
+		}
+		tx.commit();
+		session.close();
+		
+	}
 }
